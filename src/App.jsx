45bcae17685b683dbc69efb7a71bf257634160a0
Harsh Chaturvedi <harshchaturvedi1002@gmail.com>
2022-05-06 17:52:33 +0530

    Fixed Visual Bugs

--- src/App.jsx
+++ src/App.jsx
@@ -14,17 +14,19 @@ function App() {
       <section className="hero page-section">
         <h1 className="heading primary">Event Management is Hard</h1>
         <h2 className="heading secondary">We can make stuff easy for you.</h2>
-        <p>Avalible on iOS, Android and Web.</p>
-        <div className="cta-flex">
-          <button className="cta primary">Download Now</button>
-          <button className="cta">Browse Online</button>
+        <div className="content">
+          <p>Avalible on iOS, Android and Web.</p>
+          <div className="cta-flex">
+            <button className="cta white primary">Download Now</button>
+            <button className="cta white">Browse Online</button>
+          </div>
         </div>
       </section>
       <section className="page-section">
         <h1>Heading</h1>
         <p>Text</p>
-        <button className="cta white primary">Action 1</button>
-        <button className="cta white">Action 2</button>
+        <button className="cta primary">Action 1</button>
+        <button className="cta">Action 2</button>
       </section>
       <section className="page-section"></section>
     </div>
