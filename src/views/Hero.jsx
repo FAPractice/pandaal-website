4a3a35be341ba4b37a5158fd82d901ba1957b685
Harsh Chaturvedi <harshchaturvedi1002@gmail.com>
2022-05-07 10:46:35 +0530

    Added Buttons to Download App

--- src/views/Hero.jsx
+++ src/views/Hero.jsx
@@ -11,7 +11,7 @@ function Hero() {
       // animate={{ background: ["black", "hsl(349, 90%, 63%)"] }}
       // transition={{ delay: 1.2, duration: 0.5, type: "tween" }}
     >
-      <div>
+      <div className="headings">
         <motion.h1
           animate={fadeSlideBottom}
           transition={{ duration: 0.4, ease: "easeOut" }}
@@ -27,12 +27,34 @@ function Hero() {
           We make it easy for you.
         </motion.h2>
       </div>
-      <motion.p
-        animate={fadeSlideBottom}
-        transition={{ duration: 0.3, delay: 0.9, ease: "easeOut" }}
-      >
-        Availabe on iOS, Android and Web
-      </motion.p>
+      <div className="content">
+        <motion.p
+          animate={fadeSlideBottom}
+          transition={{ duration: 0.3, delay: 0.9, ease: "easeOut" }}
+        >
+          Availabe on iOS, Android and Web
+        </motion.p>
+        <div
+          className="cta-flex"
+          animate={fadeSlideBottom}
+          transition={{ delay: 1.2, duration: 0.2, ease: "easeIn" }}
+        >
+          <motion.button
+            animate={fadeSlideBottom}
+            transition={{ delay: 1, duration: 0.2, ease: "easeIn" }}
+            className="cta white primary"
+          >
+            Download Now
+          </motion.button>
+          <motion.button
+            animate={fadeSlideBottom}
+            transition={{ delay: 1.2, duration: 0.2, ease: "easeIn" }}
+            className="cta white"
+          >
+            Browse Online
+          </motion.button>
+        </div>
+      </div>
     </Section>
   );
 }
